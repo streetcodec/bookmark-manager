115f0028176e06849bb8c57e732389117df4a513
streetcodec <streetcodec@gmail.com>
2026-02-16 03:49:56 +0530

    quick fix

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -21,7 +21,7 @@ export default function AuthButton({ session }: { session: Session | null }) {
     }
 
     const handleSignIn = async () => {
-        const redirectTo = `${getURL()}auth/callback`
+        const redirectTo = `https://bookmark-manager-gilt-eta.vercel.app/auth/callback` // Temp fix
         const { error } = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
